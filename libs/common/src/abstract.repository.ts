172b57ed6e311553f38c821739e9efae97f0f346
mahdi keshavarz <69865082+ma-pk1996@users.noreply.github.com>
2025-05-19 19:56:25 +0330

    first commit

---
+++ libs/common/src/abstract.repository.ts
@@ -0,0 +1,93 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import { Logger, NotFoundException } from '@nestjs/common';
+import {
+  FilterQuery,
+  Model,
+  Types,
+  UpdateQuery,
+  SaveOptions,
+  Connection,
+} from 'mongoose';
+import { AbstractDocument } from './abstract.schema';
+
+export abstract class AbstractRepository<TDocument extends AbstractDocument> {
+  protected abstract readonly logger: Logger;
+
+  constructor(
+    protected readonly model: Model<TDocument>,
+    private readonly connection: Connection,
+  ) {}
+
+  async create(
+    document: Omit<TDocument, '_id'>,
+    options?: SaveOptions,
+  ): Promise<TDocument> {
+    const createdDocument = new this.model({
+      ...document,
+      _id: new Types.ObjectId(),
+    });
+    return (
+      await createdDocument.save(options)
+    ).toJSON() as unknown as TDocument;
+  }
+
+  async findOne(filterQuery: FilterQuery<TDocument>): Promise<TDocument> {
+    const document = (await this.model.findOne(
+      filterQuery,
+      {},
+      { lean: true },
+    )) as unknown as TDocument;
+
+    if (!document) {
+      this.logger.warn('Document not found with filterQuery', filterQuery);
+      throw new NotFoundException('document not found');
+    }
+    return document;
+  }
+
+  async findOneAndUpdate(
+    filterQuery: FilterQuery<TDocument>,
+    update: UpdateQuery<TDocument>,
+  ): Promise<TDocument> {
+    const document = (await this.model.findOneAndUpdate(filterQuery, update, {
+      lean: true,
+      new: true,
+    })) as unknown as TDocument;
+
+    if (!document) {
+      this.logger.warn('Document not found with filterQuery', filterQuery);
+      throw new NotFoundException('document not found');
+    }
+    return document;
+  }
+
+  async upsert(
+    filterQuery: FilterQuery<TDocument>,
+    document: Partial<TDocument>,
+  ): Promise<TDocument> {
+    return (await this.model.findByIdAndUpdate(filterQuery, document, {
+      lean: true,
+      upsert: true,
+      new: true,
+    })) as unknown as TDocument;
+  }
+
+  async find(filterQuery: FilterQuery<TDocument>): Promise<TDocument> {
+    return (await this.model.find(
+      filterQuery,
+      {},
+      { lean: true },
+    )) as unknown as TDocument;
+  }
+
+  async startTransaction() {
+    const session = await this.connection.startSession();
+    session.startTransaction();
+    return session;
+  }
+}
---
+++ libs/common/src/index.ts
@@ -0,0 +1,5 @@
+/* eslint-disable prettier/prettier */
+
+export * from './database.module';
+export * from './abstract.schema';
+export * from './abstract.repository';
