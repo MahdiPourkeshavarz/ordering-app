b72049416c1e8ece35e5ebc0e8c839ac7d59d16c
mahdi keshavarz <69865082+ma-pk1996@users.noreply.github.com>
2025-05-21 21:47:05 +0330

    feat(mongoose): adding mongoose config to order module

--- apps/orders/src/orders.module.ts
+++ apps/orders/src/orders.module.ts
@@ -4,6 +4,9 @@ import { OrdersService } from './orders.service';
 import { ConfigModule } from '@nestjs/config';
 import * as Joi from 'joi';
 import { DatabaseModule } from '@app/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Order, OrderSchema } from './schemas/order.schema';
+import { OrdersRepository } from './orders.repository';
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -14,8 +17,14 @@ import { DatabaseModule } from '@app/common';
       envFilePath: './apps/orders/.env',
     }),
     DatabaseModule,
+    MongooseModule.forFeature([
+      {
+        name: Order.name,
+        schema: OrderSchema,
+      },
+    ]),
   ],
   controllers: [OrdersController],
-  providers: [OrdersService],
+  providers: [OrdersService, OrdersRepository],
 })
 export class OrdersModule {}
