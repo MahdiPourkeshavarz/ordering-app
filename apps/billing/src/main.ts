26024eaa8a43b8b23e3cfb1310ab0cb26208c3f0
mahdi keshavarz <69865082+ma-pk1996@users.noreply.github.com>
2025-05-22 10:03:25 +0330

    feat(billing): implementing rabbit mq into billing app

--- apps/billing/src/main.ts
+++ apps/billing/src/main.ts
@@ -4,7 +4,8 @@ import { RmqService } from '@app/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(BillingModule);
-  const rmq = app.get<RmqService>(RmqService);
+  const rmqService = app.get<RmqService>(RmqService);
+  app.connectMicroservice(rmqService.getOptions('BILLING'));
   await app.listen(process.env.port ?? 3000);
 }
 bootstrap();
